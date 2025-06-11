49299e3e4b7a299e5966fc5c0e98a0a64eec08b7
Prince6207 <princekansal06022007@gmail.com>
2025-06-11 14:21:42 +0530

    A new c file

---
+++ new.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    printf("This is a new file in the local repository.\n");
+    printf("It demonstrates how to create and use a new C file.\n");
+    return 0;
+}
